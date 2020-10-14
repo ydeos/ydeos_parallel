66c3d14db80d647f403bd3030b7de94c764d10bc
Guillaume Florent <guillaume.florent@tutanota.com>
2020-10-14 16:27:41 +0200

    Testing all functions

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -3,7 +3,11 @@
 
 r"""Tests for the parallel.py module"""
 
-from ydeos_parallel.parallel import chunks, nb_per_process
+from typing import Callable, List, Tuple
+
+from ydeos_parallel.parallel import chunks, nb_per_process, physical_memory, \
+    processor, number_of_cpus, number_of_cores, number_of_threads, \
+    parallel_run
 
 
 def _ten_by_four_chunks_verification(chunks_):
@@ -42,3 +46,77 @@ def test_nb_per_process():
     assert nb_per_process(9, 4) == 3
     assert nb_per_process(8, 4) == 2
     assert nb_per_process(7, 4) == 2
+
+
+def test_physical_memory():
+    r"""Test that we get a positive integer."""
+    assert isinstance(physical_memory(), int)
+    assert physical_memory() > 0
+
+
+def test_processor():
+    r"""Test we get a string description"""
+    assert isinstance(processor(), str)
+    assert len(processor()) > 0
+
+
+def test_number_of_cpus():
+    r"""Test that we get a positive integer."""
+    assert isinstance(number_of_cpus(), int)
+    assert number_of_cpus() > 0
+
+
+def test_number_of_cores():
+    r"""Test that we get a positive integer."""
+    assert isinstance(number_of_cores(), int)
+    assert number_of_cores() > 0
+
+
+def test_number_of_threads():
+    r"""Test that we get a positive integer."""
+    assert isinstance(number_of_threads(), int)
+    assert number_of_threads() > 0
+
+
+def __sample_iter_func(atomic_func: Callable,
+                       cases: List,
+                       other_args: List,
+                       process_nb: int = -1):
+    r"""Sample iteration function."""
+    for j, case in enumerate(cases):
+        atomic_func(case, *other_args)
+
+
+def __sample_atomic_func(case: Tuple, p_1, p_2, p_3="Z"):
+    r"""Sample 'atomic' procedure that actually does something with a case.
+
+    Parameters
+    ----------
+    case : tuple
+    p_1 : whatever, specific to this function
+    p_2 : whatever, specific to this function
+    p_3 : whatever, specific to this function
+
+    """
+    my_list = []
+
+    # here the function can handle a case with any number of params,
+    # which may not always be so
+    for param in case:
+        my_list.append(param)
+    my_list.append(p_1)
+    my_list.append(p_2)
+    my_list.append(p_3)
+
+
+def test_parallel_run():
+    r"""Test parallel run happy path"""
+    list_1 = ['a', 'b', 'c', 'd']
+    list_2 = ['1', '2', '3', '4']
+    list_3 = ['+', '-', '*', '/']
+
+    assert parallel_run(iter_func=__sample_iter_func,
+                        atomic_func=__sample_atomic_func,
+                        iter_args=[list_1, list_2, list_3],
+                        args=['lorem', "ipsum", "dolor"],
+                        nb_cores=number_of_cores()) is None
